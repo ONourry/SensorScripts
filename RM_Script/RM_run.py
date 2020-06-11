efb2b2ada5958273c6238524c672b9bc1826ecc0
ONourry <onourry3@gmail.com>
2020-06-11 22:13:51 +0900

    Changed from psql to file to track apps

--- RM_Script/RM_run.py
+++ RM_Script/RM_run.py
@@ -1,27 +1,27 @@
 import os
 import subprocess
 import sys
-import psycopg2 as psycopg2
-
-cursor=None
-connection=None
-try:
-    connection = psycopg2.connect(user=os.environ.get('POSTGRES_REFACTORING_USER'),
-                                  password=os.environ.get('POSTGRES_REFACTORING_PW'),
-                                  host="127.0.0.1",
-                                  port="5432",
-                                  database="EnergyConsumption")
-    cursor = connection.cursor()
-    # Print PostgreSQL Connection properties
-    print(connection.get_dsn_parameters(), "\n")
-
-    # Print PostgreSQL version
-    cursor.execute("SELECT version();")
-    record = cursor.fetchone()
-    print("You are connected to - ", record, "\n")
-
-except (Exception) as error:
-    print("Error while connecting to PostgreSQL", error)
+# import psycopg2 as psycopg2
+#
+# cursor=None
+# connection=None
+# try:
+#     connection = psycopg2.connect(user=os.environ.get('POSTGRES_REFACTORING_USER'),
+#                                   password=os.environ.get('POSTGRES_REFACTORING_PW'),
+#                                   host="127.0.0.1",
+#                                   port="5432",
+#                                   database="EnergyConsumption")
+#     cursor = connection.cursor()
+#     # Print PostgreSQL Connection properties
+#     print(connection.get_dsn_parameters(), "\n")
+#
+#     # Print PostgreSQL version
+#     cursor.execute("SELECT version();")
+#     record = cursor.fetchone()
+#     print("You are connected to - ", record, "\n")
+#
+# except (Exception) as error:
+#     print("Error while connecting to PostgreSQL", error)
 
 
 def setup_env():
@@ -79,15 +79,20 @@ def deleteRepoClone(projectName,repo_dir):
 
 
 
-def update_rm_column(source):
-    query = """UPDATE public.app_data SET rm_executed={executed} WHERE source=\'{source}\';""".format(
-        executed=1, source=source)
-    print(query)
+# def update_rm_column(source):
+#     query = """UPDATE public.app_data SET rm_executed={executed} WHERE source=\'{source}\';""".format(
+#         executed=1, source=source)
+#     print(query)
+#
+#     cursor.execute(query)
+#
+#
+#     connection.commit()
 
-    cursor.execute(query)
 
-
-    connection.commit()
+def track_rm_executed_apps(source,filepath):
+    with open(filepath,"a+") as file:
+        file.write(source)
 
 
 def main():
@@ -95,7 +100,14 @@ def main():
     repo_clone_dir = "/home/olivier/clone_dir"
     output_dir = "/home/olivier/output"
 
+    cur_dir = os.getcwd()
+    executed_1 = os.path.join(cur_dir,'executed_1.txt')
+    executed_2 = os.path.join(cur_dir, 'executed_2.txt')
+    executed_3 = os.path.join(cur_dir, 'executed_3.txt')
+    executed_4 = os.path.join(cur_dir, 'executed_4.txt')
+
     list_id = sys.argv[1]
+    list_id = int(list_id)
     #setup_env()
     #generateRMExec()
 
@@ -110,9 +122,18 @@ def main():
 
         deleteRepoClone(projectName,repo_clone_dir)
 
-        update_rm_column(project)
+        if list_id == 1:
+            track_rm_executed_apps(project,executed_1)
+        elif list_id == 2:
+            track_rm_executed_apps(project,executed_2)
+        elif list_id == 3:
+            track_rm_executed_apps(project,executed_3)
+        elif list_id == 4:
+            track_rm_executed_apps(project,executed_4)
 
-    connection.close()
+        #update_rm_column(project)
+
+    #connection.close()
 
 if __name__ == "__main__":
     main()
